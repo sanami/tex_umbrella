80c142bfbe13d7c8c1e113e6327b48896047b3ae
sa <sanami77@gmail.com>
2023-07-07 03:59:16 +0600

    Install tailwind

--- apps/tex_web/mix.exs
+++ apps/tex_web/mix.exs
@@ -48,7 +48,8 @@ defmodule TexWeb.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:tex, in_umbrella: true},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
@@ -59,7 +60,7 @@ defmodule TexWeb.MixProject do
     [
       setup: ["deps.get"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
