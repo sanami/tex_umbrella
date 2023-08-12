4ba3814d0d068149655d9bf838efbd932eae7b4c
sa <sanami77@gmail.com>
2023-08-13 02:35:07 +0600

    Call removeEventListener on component destroy

--- apps/tex_web/assets/js/hooks.js
+++ apps/tex_web/assets/js/hooks.js
@@ -1,22 +1,31 @@
+let paginate_el = null
+
+const navigate = function(ev) {
+  pp("PageNav.navigate")
+
+  if (ev.isTrusted) {
+    const pos = paginate_el.getBoundingClientRect()
+    pp("PageNav.click", ev.x)
+
+    if (ev.x < pos.left) {
+      document.querySelector(".page-prev").click()
+    } else if (ev.x > pos.right) {
+      document.querySelector(".page-next").click()
+    }
+  }
+}
+
 export const Hooks = {
   PageNav: {
     mounted() {
       pp("PageNav.mounted")
-      const main_el = window
-      const content_el = this.el
+      paginate_el = this.el
+      window.addEventListener("click", navigate)
+    },
 
-      main_el.addEventListener("click", (ev) => {
-        if (ev.isTrusted) {
-          const pos = content_el.getBoundingClientRect()
-          pp("PageNav.click", ev.x, pos)
-          
-          if (ev.x < pos.left) {
-            document.querySelector(".page-prev").click()
-          } else if (ev.x > pos.right) {
-            document.querySelector(".page-next").click()
-          }
-        }
-      })
+    destroyed() {
+      pp("PageNav.destroyed")
+      window.removeEventListener("click", navigate)
     },
 
     updated() {
